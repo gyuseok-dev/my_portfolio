392782cba3a7945a22dc04b8d22b7049fa72767b
Jim <gyskjng@gmail.com>
2023-02-11 23:01:43 +0900

    feat: vue 버튼 만들기

---
+++ src/client/src/views/ButtonView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(1)
+const increment = () => {
+  count.value++
+}
+</script>
+
+
+<template>
+  <div id="app">
+    <button @click="increment">count is: {{ count }}</button>
+  </div>
+</template>
