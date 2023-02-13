b6762263deebfa93a7bd3cde4031ee5936594526
Jim <gyskjng@gmail.com>
2023-02-14 02:19:19 +0900

    test

--- src/client/src/views/ButtonView.vue
+++ src/client/src/views/ButtonView.vue
@@ -5,11 +5,32 @@ const count = ref(1)
 const increment = () => {
   count.value++
 }
+
+const items = [{ message: 'Foo' }, { message: 'Bar' }]
 </script>
 
 
 <template>
   <div id="app">
     <button @click="increment">count is: {{ count }}</button>
+  </div>
+  <div>
+    <ul>
+    <li v-for="item in items">{{ item.message }}</li>
+    </ul>
+  </div>
+  <div id="circle">
+        <v-stage ref="stage">
+      <v-layer>
+      <v-circle :config="{
+          x: 200,
+          y: 100,
+          radius: 50,
+          fill: 'green'
+        }"
+      />
+      </v-layer>
+      <v-layer ref="dragLayer"></v-layer>
+    </v-stage>
   </div>
 </template>
