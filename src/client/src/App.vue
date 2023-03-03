05c5e9feac3f992029ce255abab4099616663ae0
Jim <gyskjng@gmail.com>
2023-03-03 14:15:24 +0900

    feat: Canvas 추가

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -9,6 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/button">Button</RouterLink>
+        <RouterLink to="/canvas">Canvas</RouterLink>
       </nav>
     </div>
   </header>
--- src/client/src/router/index.ts
+++ src/client/src/router/index.ts
@@ -24,7 +24,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ButtonView.vue')
+    },
+    {
+      path: '/canvas',
+      name: 'Canvas',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/CanvasView.vue')
     }
+
 
   ]
 })
