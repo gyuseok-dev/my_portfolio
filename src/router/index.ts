ba3ba7459737a07a6b8d713fbec8a61e8ec4a665
Jim <gyskjng@gmail.com>
2023-07-04 20:43:42 +0900

    feat: add quizes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 // import CounterView from '../views/study/CounterView.vue'
 import NoteView from '../views/study/NoteView.vue'
+import QuizView from '../views/study/QuizView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +11,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: NoteView
+      component: QuizView
     },
     {
       path: '/hole',
