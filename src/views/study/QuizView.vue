5845eb7bd2024afe03744de6818bad8d703d0796
Jim <gyskjng@gmail.com>
2023-07-05 20:18:58 +0900

    feat: quiz 컴포넌트 분리

--- src/views/study/QuizView.vue
+++ src/views/study/QuizView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import q from "../../data/quizes.json"
 import { ref, watch } from "vue"
+import Card from "../../components/study/Card.vue"
 const quizes = ref(q)
 const search = ref("")
 watch(search, () => {
@@ -15,13 +16,7 @@ watch(search, () => {
             <input v-model="search" type="text" placeholder="Search...">
         </header>
         <div class="options-container">
-            <div v-for="quiz in quizes" :key="quiz.id" class="card">
-                <img :src="quiz.img" alt="">
-                <div class="card-text">
-                    <h2>{{ quiz.name }}</h2>
-                    <p>{{ quiz.questions.length }} questions</p>
-                </div>
-            </div>
+            <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
         </div>
     </div>
 </template>
@@ -59,24 +54,5 @@ header input {
 }
 
 /* CARD */
-.card {
-    width: 310px;
-    overflow: hidden;
-    border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(255, 255, 255, 0.1);
-    margin-bottom: 35px;
-    margin-right: 20px;
-    cursor: pointer;
 
-}
-
-.card img {
-    width: 100%;
-    height: 190px;
-    margin: 0;
-}
-
-.card .card-text h2 {
-    font-weight: bold;
-}
 </style>
