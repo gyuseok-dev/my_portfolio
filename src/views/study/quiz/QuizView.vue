27f16a893ba018204b93be5ac3251c9a912a4d47
Jim <gyskjng@gmail.com>
2023-07-15 21:01:33 +0900

    refactor: watch 대신 computed 사용

--- src/views/study/quiz/QuizView.vue
+++ src/views/study/quiz/QuizView.vue
@@ -2,7 +2,7 @@
 import Question from "@/components/study/Question.vue"
 import QuizHeader from "@/components/study/QuizHeader.vue"
 import { useRoute } from "vue-router";
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import quizes from "@/data/quizes.json"
 const route = useRoute();
 console.log(route.params.id)
@@ -10,16 +10,19 @@ const quizId = parseInt(route.params.id as string);
 const quiz = quizes.find((quiz) => quiz.id === quizId);
 const currentQuestionIndex = ref(0)
 
-const questionStatus = ref(`${currentQuestionIndex.value}/${quiz!.questions.length}`)
+// const questionStatus = ref(`${currentQuestionIndex.value}/${quiz!.questions.length}`)
 
-// IMPORTANT: watch() is a Vue 3 function, not a Vue 2 function
-watch(() => currentQuestionIndex.value, () => {
-    questionStatus.value = `${currentQuestionIndex.value + 1}/${quiz!.questions.length}`
-})
+// // IMPORTANT: watch() is a Vue 3 function, not a Vue 2 function
+// watch(() => currentQuestionIndex.value, () => {
+//     questionStatus.value = `${currentQuestionIndex.value + 1}/${quiz!.questions.length}`
+// })
+// this is simplified with computed()
+const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz!.questions.length}`)
+
 </script>
 <template>
     <div>
-        <QuizHeader :questionStatus="questionStatus"/>
+        <QuizHeader :questionStatus="questionStatus" />
         <div>
             <Question :question="quiz!.questions[currentQuestionIndex]" />
         </div>
